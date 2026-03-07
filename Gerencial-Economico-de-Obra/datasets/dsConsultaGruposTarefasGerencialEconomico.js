6d4554dadcc5094df598f34e11df31681b3ca0e7
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2026-03-07 15:46:01 -0300

    Incluí a consulta de grupos de Atividades

---
+++ Gerencial-Economico-de-Obra/datasets/dsConsultaGruposTarefasGerencialEconomico.js
@@ -0,0 +1,192 @@
+function createDataset(fields, constraints, sortFields) {
+    try {
+        var constraints = getConstraints(constraints);
+        lancaErroSeConstraintsObrigatoriasNaoInformadas(constraints, ["CODCOLIGADA", "IDPRJ"]);
+
+        var query = "";
+        query += "SELECT * FROM ZVWPLANILHA ";
+        query += "WHERE ";
+        query += "CODCOLIGADA = ? ";
+        query += "AND IDPRJ = ?  ";
+        query += "AND SERVICO = 0 ";
+        query += "AND TIPOPLANILHA = 1 ";
+        query += "AND NIVEL > 1 ";
+
+
+        var retorno = executaQuery(query,[
+            {type:"int", value:constraints.CODCOLIGADA},
+            {type:"int", value:constraints.IDPRJ},
+        ],"/jdbc/FluigRM");
+
+        log.dir(retorno);
+        return returnDataset("SUCCESS", "", JSON.stringify(retorno));
+
+
+    } catch (error) {
+        if (typeof error == "object") {
+            var mensagem = "";
+            var keys = Object.keys(error);
+            for (var i = 0; i < keys.length; i++) {
+                mensagem += (keys[i] + ": " + error[keys[i]]) + " - ";
+            }
+            log.info("Erro ao executar Dataset:");
+            log.dir(error);
+            log.info(mensagem);
+
+            return returnDataset("ERRO", mensagem, null);
+        } else {
+            return returnDataset("ERRO", error, null);
+        }
+    }
+}
+
+
+
+
+// Utils
+function getConstraints(constraints) {
+    var retorno = {};
+    if (constraints != null) {
+        for (var i = 0; i < constraints.length; i++) {
+            var constraint = constraints[i];
+            retorno[constraint.fieldName] = constraint.initialValue;
+        }
+    }
+    return retorno;
+}
+function returnDataset(STATUS, MENSAGEM, RESULT) {
+    var dataset = DatasetBuilder.newDataset();
+    dataset.addColumn("STATUS");
+    dataset.addColumn("MENSAGEM");
+    dataset.addColumn("RESULT");
+    dataset.addRow([STATUS, MENSAGEM, RESULT]);
+    return dataset;
+}
+function lancaErroSeConstraintsObrigatoriasNaoInformadas(constraints, listConstrainstObrigatorias) {
+    try {
+        var retornoErro = [];
+        for (var i = 0; i < listConstrainstObrigatorias.length; i++) {
+            if (constraints[listConstrainstObrigatorias[i]] == null || constraints[listConstrainstObrigatorias[i]] == "" || constraints[listConstrainstObrigatorias[i]] == undefined) {
+                retornoErro.push(listConstrainstObrigatorias[i]);
+            }
+        }
+
+        if (retornoErro.length > 0) {
+            throw "Constraints obrigatorias nao informadas (" + retornoErro.join(", ") + ")";
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+function catchError(error) {
+    var msg = "";
+
+    // Try to extract useful message safely
+    if (error && error.javaException) {
+        msg = error.javaException.getMessage();
+    } else if (error && error.message) {
+        if (error.message.Error) {
+        } else {
+            msg = error.message;
+        }
+    } else {
+        msg = String(error);
+    }
+
+    log.error("ERRO==============> " + msg);
+    log.error("Type of error: " + typeof error);
+    log.error("Type of msg: " + typeof msg);
+
+    // Safely rethrow as standard JS error
+    return "Erro ao executar Dataset: " + msg;
+}
+function getDateNow() {
+    var date = new Date();
+    var dia = date.getDate();
+    if (dia < 10) {
+        dia = "0" + dia;
+    }
+    var mes = date.getMonth() + 1;
+    if (mes < 10) {
+        mes = "0" + mes;
+    }
+
+    var ano = date.getFullYear();
+
+    var dateTime = [ano, mes, dia].join("-");
+    return dateTime;
+}
+function executaQuery(query, constraints, dataSorce) {
+    try {
+        var dataSource = dataSorce;
+        var ic = new javax.naming.InitialContext();
+        var ds = ic.lookup(dataSource);
+
+        var conn = ds.getConnection();
+        var stmt = conn.prepareStatement(query);
+
+        var counter = 1;
+        for (var i = 0; i < constraints.length; i++) {
+            var val = constraints[i];
+            if (val.type == "int") {
+                stmt.setInt(counter, val.value);
+            }
+            else if (val.type == "float") {
+                stmt.setFloat(counter, val.value);
+            }
+            else if (val.type == "date") {
+                stmt.setString(counter, val.value);
+            }
+            else if (val.type == "datetime") {
+                stmt.setString(counter, val.value);
+            } else {
+                stmt.setString(counter, val.value);
+            }
+            counter++;
+        }
+
+        var rs = stmt.executeQuery();
+        var columnCount = rs.getMetaData().getColumnCount();
+        var retorno = [];
+
+        while (rs.next()) {
+            var linha = {};
+            for (var j = 1; j < columnCount + 1; j++) {
+                linha[rs.getMetaData().getColumnName(j)] = rs.getObject(rs.getMetaData().getColumnName(j)) + "";
+            }
+            retorno.push(linha);
+        }
+
+        return retorno;
+
+    } catch (error) {
+            var msg = "";
+            // Try to extract useful message safely
+            if (error && error.javaException) {
+                msg = error.javaException.getMessage();
+            } else if (error && error.message) {
+                if (error.message.Error) {
+                }else{
+                    msg = error.message;
+                }
+
+
+            } else {
+                msg = String(error);
+            }
+
+            log.error("ERRO==============> " + msg);
+            log.error("Type of error: " + typeof error);
+            log.error("Type of msg: " + typeof msg);
+
+            // Safely rethrow as standard JS error
+            throw "Erro ao executar Dataset: " + msg;
+    } finally {
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (conn != null) {
+            conn.close();
+        }
+    }
+}
